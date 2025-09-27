2d6e035c5f55aa5c77a8abecc7c410b7292ca30e
notyouradhee <guptaadit2004@gmail.com>
2025-09-28 01:02:42 +0545

    pass with pilot

--- src/simplify.py
+++ src/simplify.py
@@ -2,4 +2,14 @@
 
 def simplify_path(path):
     # TODO: use a stack of directory names to normalize an absolute path
-    raise NotImplementedError
+    parts = path.split('/')
+    stack = []
+    for part in parts:
+        if part == '' or part == '.':
+            continue
+        elif part == '..':
+            if stack:
+                stack.pop()
+        else:
+            stack.append(part)
+    return '/' + '/'.join(stack)
